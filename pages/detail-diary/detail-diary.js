e2bc399008a9bb9776058a6609e82b5df548d21f
HuziG <1067408814@qq.com>
2019-05-09 09:12:04 +0800

    -v 1.0.7

--- pages/detail-diary/detail-diary.js
+++ pages/detail-diary/detail-diary.js
@@ -1,22 +1,10 @@
 // pages/detail-diary/detail-diary.js
-/**
- * 1.通过options，获取参数date，time，place，content
- * 2.通过localstorage，获取openid
- * 3.传递
- */
-
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
                   
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     let item = JSON.parse(options.item)
     this.setData({
